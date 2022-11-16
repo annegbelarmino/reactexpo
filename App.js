6f7e46c886d8f3e63cad4ac8855b5943d51e2b0f
Anne <annegvbelarmino@fac.pe.senac.br>
2022-11-16 09:57:53 -0300

    inicial commit

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'; 
+import lili from './src/images/lili.jpeg'
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Image source={lili} style={{width: 200, height:200}}/>
       <StatusBar style="auto" />
     </View>
   );
